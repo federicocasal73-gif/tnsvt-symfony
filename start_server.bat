e0d922995a67dccb88430c351b1f436833b22f2d
federicocasal73-gif <federicocasal73@gmail.com>
2026-06-13 08:21:44 -0300

    chore(backend): activar FIREBASE_CREDENTIALS_PATH + start_server.bat
    - .env: descomentar FIREBASE_CREDENTIALS_PATH (push FCM end-to-end OK)
    - start_server.bat: helper para arrancar dev server desde ruta con espacios
    
    Verificado en Galaxy Z Fold 6 (user=ADMIN01, task='Push FCM test 2'):
    backend log: Firebase Admin SDK initialized
    device.last_seen_at actualizado al momento del send
    push notification visible en el device

---
+++ start_server.bat
@@ -0,0 +1,3 @@
+@echo off
+cd /d C:\Users\HP 240 inch G9\tnsvt-symfony
+"C:\Users\HP 240 inch G9\AppData\Local\Microsoft\WinGet\Packages\PHP.PHP.8.4_Microsoft.Winget.Source_8wekyb3d8bbwe\php.exe" -S 0.0.0.0:8000 -t "C:\Users\HP 240 inch G9\tnsvt-symfony\public" "C:\Users\HP 240 inch G9\tnsvt-symfony\public\router.php"
